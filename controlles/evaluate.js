364bf488df4d0092e0c259455d0d3989a3448ec9
Yoandry Pacheco Aguila <yoandrypa@gmail.com>
2016-11-25 15:47:38 -0500

    integration with cenit-io

--- controlles/evaluate.js
+++ controlles/evaluate.js
@@ -1,22 +1,49 @@
 module.exports = function (req, res, params) {
-    var parameters = params.parameters || params.params || {};
+    var parameters = params.parameters || params.params || {},
+        access_key = req.header('x-user-access-key'),
+        access_token = req.header('x-user-access-token'),
+        api_path = process.env.CENIT_IO_API_PATH,
+        cenit_io = require('../libs/cenit-io')(api_path, access_key, access_token),
+
+        run = function (code) {
+            var vm = require('vm'),
+                context = { require: require, cenit_io: cenit_io, params: parameters },
+                result;
+
+            try {
+                result = vm.runInNewContext(code, context, 'stack.vm');
+                res.json(result);
+            } catch (e) {
+                res.status(500).send(e.stack);
+            }
+        },
+
+        get_and_run = function (id) {
+            cenit_io.algorithm(id, function (err, alg) {
+                if (err) return res.status(500).send(err);
+
+                cenit_io.snippet(alg.snippet.id, function (err, snippet) {
+                    if (err) return res.status(500).send(err);
+
+                    run(snippet.code);
+                });
+            });
+        };
 
     if (typeof parameters == 'string') {
         parameters = JSON.parse(parameters.trim() || '{}');
     }
 
-    const
-        vm = require('vm'),
-        context = {
-            require: require,
-            cenit_io: {
-                access_key: params.access_key || process.env.CENIT_IO_ACCESS_KEY,
-                access_token: params.access_token || process.env.CENIT_IO_ACCESS_TOKEN
-            },
-            params: parameters
-        };
+    if (params.id) {
+        get_and_run(params.id);
+    } else {
+        var code = params.code || '',
+            match = code.match(/^@id:([a-f0-9]+)$/i);
 
-    const result = vm.runInNewContext(params.code, context, 'stack.vm');
-
-    res.json(result);
+        if (match) {
+            get_and_run(match[1]);
+        } else {
+            run(code);
+        }
+    }
 };
--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
+    env = require('node-env-file'),
+    path = require('path'),
     app = express();
 
-
+env(path.join(__dirname, '.env'));
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+app.use('/public', express.static(__dirname + '/public'));
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
@@ -16,7 +19,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function (request, response) {
-    response.render('pages/index');
+    require('./controlles/index')(request, response);
 });
 
 app.post('/', function (request, response) {
