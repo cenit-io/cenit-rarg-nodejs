35b2f6f0fc534aea88c10cd8065e7d877faa9bc3
Yoandry Pacheco Aguila <yoandrypa@gmail.com>
2016-11-28 11:31:34 -0500

    use cdn

--- index.js
+++ index.js
@@ -1,35 +1,99 @@
-var express = require('express'),
+// Include the cluster module
+var cluster = require('cluster'),
+    express = require('express'),
     bodyParser = require('body-parser'),
     env = require('node-env-file'),
     path = require('path'),
     fs = require('fs'),
-    envFile = path.join(__dirname, '.env'),
-    app = express();
+    envFile = path.join(__dirname, '.env');
 
 if (fs.existsSync(envFile)) env(envFile);
 
-app.set('port', (process.env.PORT || 5000));
+// Code to run if we're in the master process
+if (cluster.isMaster) {
 
-app.use(express.static(__dirname + '/public'));
-app.use('/public', express.static(__dirname + '/public'));
+    var maxWorkers = 4,
 
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+        // Handle worker messages
+        onMessage = function (message) {
+            console.log('We got a message!');
+            console.log(message);
+        },
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+        // Check workers to restart
+        checkWorkers = function () {
+            for (var i in cluster.workers) {
+                if (cluster.workers[i].toRestart && cluster.workers[i].state === 'listening') {
+                    cluster.workers[i].disconnect();
+                }
+            }
+        },
 
-app.get('/', function (request, response) {
-    require('./controlles/index')(request, response);
-});
+        // Create worker
+        createWorker = function () {
+            var worker = cluster.fork();
+            worker.on('message', onMessage);
+            worker.on('online', function () {
+                console.info('Worker #' + worker.id + ' is online.');
+                checkWorkers();
+            });
+            return worker;
+        };
 
-app.post('/', function (request, response) {
-    require('./controlles/evaluate')(request, response, request.body);
-});
+    // Create a worker for each CPU
+    for (var i = 0; i < maxWorkers; i++) {
+        createWorker();
+    }
 
-app.listen(app.get('port'), function () {
-    console.log('Node app is running on port', app.get('port'));
-});
+    // Listen for dying workers
+    cluster.on('exit', function (worker) {
+        // Replace the dead worker, we're not sentimental
+        if (worker.suicide) {
+            console.warn('Worker ' + worker.id + ' restarted.');
+        } else {
+            console.error('Worker ' + worker.id + ' died.');
+        }
+        createWorker();
+    });
+
+    // Disconnect
+    cluster.on('disconnect', function (worker) {
+        console.warn('Disconnect, restarting worker #' + worker.id);
+        worker.kill();
+    });
+
+} else {
+    // Code to run if we're in a worker process
+    var app = express()
+        worker = cluster.worker;
+
+    app.set('port', (process.env.PORT || 5000));
+
+    app.use(express.static(__dirname + '/public'));
+    app.use('/public', express.static(__dirname + '/public'));
+
+    app.use(bodyParser.json()); // support json encoded bodies
+    app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+    // views is directory for all template files
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
+
+    app.get('/', function (request, response) {
+        console.log('Worker #%d process GET request.', worker.id);
+        require('./controlles/index')(request, response);
+    });
+
+    app.post('/', function (request, response) {
+        console.log('Worker #%d process POST request.', worker.id);
+        require('./controlles/evaluate')(request, response, request.body);
+    });
+
+    app.listen(app.get('port'), function () {
+        console.log('Worker #%d is running on port %d.', worker.id, app.get('port'));
+    });
+}
+
+
 
 
